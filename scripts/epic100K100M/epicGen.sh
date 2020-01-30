3458d862bb481ecbcd63b0424cb1f186c6013830
ArashBayatDev <Arash.Bayat@csiro.au>
2020-01-29 19:04:38 -0800

    bug fix

--- scripts/epic100K100M/epicGen.sh
+++ scripts/epic100K100M/epicGen.sh
@@ -5,5 +5,5 @@
 g++ -O3 gendata.cpp -o gendata
 
 ./gendata 100000 10000 10000 15 123 0.001 sample.csv  | pv | pbzip2 -p95 | aws s3 cp - s3://variant-spark/GigaScience/Data/epic100K100M/s100K.v100M.csv.bz2
-bzip2 -p95 sample.csv
+pbzip2 -p95 sample.csv
 aws s3 cp sample.csv.bz2 s3://variant-spark/GigaScience/Data/epic100K100M/
