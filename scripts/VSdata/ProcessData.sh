3a2e37fd1c9fd44bbefecf2a7414f936b4878a8d
ArashBayatDev <Arash.Bayat@csiro.au>
2020-01-29 16:27:41 -0800

    improve the submit steps script

--- scripts/VSdata/ProcessData.sh
+++ scripts/VSdata/ProcessData.sh
@@ -23,21 +23,23 @@ numSample=10000
 # for nothing
 # do
 	echo "========================="
-	echo "Variants:	$numVariant"
-	echo "Samples:	$numSample"
 
 	size=$((numSample*numVariant))
-	seed=$((maxDepth*123))
+	seed=12345
+	echo "seed: " $seed
 
 
 	prefix=dataset.$numSample.$numVariant
 	prefixOut=$experiment
+	echo "output: " $prefixOut
+	echo "-------------------------"
 
 	JSON_FMT='[{"Args":["spark-submit","--deploy-mode","client","--class","au.csiro.variantspark.cli.VariantSparkApp","/home/hadoop/biospark/lib/python3.6/site-packages/varspark/jars/variant-spark_2.11-0.3.0-SNAPSHOT-all.jar","importance","-io","{\"defVariableType\":\"ORDINAL(3)\"}","-sp","%s","-sr","%s","-v","-ro","-rn","%s","-rbs","%s","-rmtf","%s","-it","csv","-if","%s","-fc","lable","-ff","%s","-of","%s","-on","1000000","-om","%s","-omf","json"],"Type":"CUSTOM_JAR","ActionOnFailure":"CONTINUE","Jar":"command-runner.jar","Properties":"","Name":"%s"}]'
 
 	step=$(printf "$JSON_FMT" "$sparkParallel" "$seed" "$numTree" "$batchSize" "$mtryFraction" "$S3$prefix.csv.bz2" "$S3$prefix.pheno.no_.csv" "$S3R$prefixOut.vsis.csv" "$S3R$prefixOut.rf.json" "$prefixOut")
 
 	echo $step
+	echo "-------------------------"
 
 	aws emr add-steps --cluster-id $clusterID --steps $step
 
@@ -58,21 +60,23 @@ numSample=10000
 for maxDepth in 3 5 7 9 11 13 15 20 25 100
 do
 	echo "========================="
-	echo "Variants:	$numVariant"
-	echo "Samples:	$numSample"
 
 	size=$((numSample*numVariant))
 	seed=$((maxDepth*123))
+	echo "seed: " $seed
 
 
 	prefix=dataset.$numSample.$numVariant
-	prefixOut=$experiment.md$maxDepth
+	prefixOut=$(printf "$experiment.rmd%03d" $maxDepth)
+	echo "output: " $prefixOut
+	echo "-------------------------"
 
 	JSON_FMT='[{"Args":["spark-submit","--deploy-mode","client","--class","au.csiro.variantspark.cli.VariantSparkApp","/home/hadoop/biospark/lib/python3.6/site-packages/varspark/jars/variant-spark_2.11-0.3.0-SNAPSHOT-all.jar","importance","-io","{\"defVariableType\":\"ORDINAL(3)\"}","-sp","%s","-sr","%s","-v","-ro","-rn","%s","-rbs","%s","-rmtf","%s","-it","csv","-if","%s","-fc","lable","-ff","%s","-of","%s","-on","1000000","-om","%s","-omf","json","-rmd","%s"],"Type":"CUSTOM_JAR","ActionOnFailure":"CONTINUE","Jar":"command-runner.jar","Properties":"","Name":"%s"}]'
 
 	step=$(printf "$JSON_FMT" "$sparkParallel" "$seed" "$numTree" "$batchSize" "$mtryFraction" "$S3$prefix.csv.bz2" "$S3$prefix.pheno.no_.csv" "$S3R$prefixOut.vsis.csv" "$S3R$prefixOut.rf.json" "$maxDepth" "$prefixOut")
 
 	echo $step
+	echo "-------------------------"
 
 	aws emr add-steps --cluster-id $clusterID --steps $step
 
@@ -93,21 +97,23 @@ numSample=10000
 for minSample in 5 10  50 100 500 1000
 do
 	echo "========================="
-	echo "Variants:	$numVariant"
-	echo "Samples:	$numSample"
 
 	size=$((numSample*numVariant))
 	seed=$((minSample*321))
+	echo "seed: " $seed
 
 
 	prefix=dataset.$numSample.$numVariant
-	prefixOut=$experiment.ms$minSample
+	prefixOut=$(printf "$experiment.rmns%04d" $minSample)
+	echo "output: " $prefixOut
+	echo "-------------------------"
 
 	JSON_FMT='[{"Args":["spark-submit","--deploy-mode","client","--class","au.csiro.variantspark.cli.VariantSparkApp","/home/hadoop/biospark/lib/python3.6/site-packages/varspark/jars/variant-spark_2.11-0.3.0-SNAPSHOT-all.jar","importance","-io","{\"defVariableType\":\"ORDINAL(3)\"}","-sp","%s","-sr","%s","-v","-ro","-rn","%s","-rbs","%s","-rmtf","%s","-it","csv","-if","%s","-fc","lable","-ff","%s","-of","%s","-on","1000000","-om","%s","-omf","json","-rmns","%s"],"Type":"CUSTOM_JAR","ActionOnFailure":"CONTINUE","Jar":"command-runner.jar","Properties":"","Name":"%s"}]'
 
 	step=$(printf "$JSON_FMT" "$sparkParallel" "$seed" "$numTree" "$batchSize" "$mtryFraction" "$S3$prefix.csv.bz2" "$S3$prefix.pheno.no_.csv" "$S3R$prefixOut.vsis.csv" "$S3R$prefixOut.rf.json" "$minSample" "$prefixOut")
 
 	echo $step
+	echo "-------------------------"
 
 	aws emr add-steps --cluster-id $clusterID --steps $step
 
@@ -128,21 +134,23 @@ numSample=10000
 for numTree in 100 200 400 800 1600
 do
 	echo "========================="
-	echo "Variants:	$numVariant"
-	echo "Samples:	$numSample"
 
 	size=$((numSample*numVariant))
-	seed=$((ntree*13))
+	seed=$((numtree*13))
+	echo "seed: " $seed
 
 
 	prefix=dataset.$numSample.$numVariant
-	prefixOut=$experiment.nt$numTree
+	prefixOut=$(printf "$experiment.rn%04d" $numTree)
+	echo "output: " $prefixOut
+	echo "-------------------------"
 
 	JSON_FMT='[{"Args":["spark-submit","--deploy-mode","client","--class","au.csiro.variantspark.cli.VariantSparkApp","/home/hadoop/biospark/lib/python3.6/site-packages/varspark/jars/variant-spark_2.11-0.3.0-SNAPSHOT-all.jar","importance","-io","{\"defVariableType\":\"ORDINAL(3)\"}","-sp","%s","-sr","%s","-v","-ro","-rn","%s","-rbs","%s","-rmtf","%s","-it","csv","-if","%s","-fc","lable","-ff","%s","-of","%s","-on","1000000","-om","%s","-omf","json","-rmns","%s","-rmd","%s"],"Type":"CUSTOM_JAR","ActionOnFailure":"CONTINUE","Jar":"command-runner.jar","Properties":"","Name":"%s"}]'
 
 	step=$(printf "$JSON_FMT" "$sparkParallel" "$seed" "$numTree" "$batchSize" "$mtryFraction" "$S3$prefix.csv.bz2" "$S3$prefix.pheno.no_.csv" "$S3R$prefixOut.vsis.csv" "$S3R$prefixOut.rf.json" "$minSample" "$maxDepth" "$prefixOut")
 
 	echo $step
+	echo "-------------------------"
 
 	aws emr add-steps --cluster-id $clusterID --steps $step
 
@@ -163,21 +171,23 @@ numSample=10000
 for batchSize in 10 50 100 500 1000
 do
 	echo "========================="
-	echo "Variants:	$numVariant"
-	echo "Samples:	$numSample"
 
 	size=$((numSample*numVariant))
 	seed=$((batchSize*7))
+	echo "seed: " $seed
 
 
 	prefix=dataset.$numSample.$numVariant
-	prefixOut=$experiment.bs$batchSize
+	prefixOut=$(printf "$experiment.rbs%04d" $batchSize)
+	echo "output: " $prefixOut
+	echo "-------------------------"
 
 	JSON_FMT='[{"Args":["spark-submit","--deploy-mode","client","--class","au.csiro.variantspark.cli.VariantSparkApp","/home/hadoop/biospark/lib/python3.6/site-packages/varspark/jars/variant-spark_2.11-0.3.0-SNAPSHOT-all.jar","importance","-io","{\"defVariableType\":\"ORDINAL(3)\"}","-sp","%s","-sr","%s","-v","-ro","-rn","%s","-rbs","%s","-rmtf","%s","-it","csv","-if","%s","-fc","lable","-ff","%s","-of","%s","-on","1000000","-om","%s","-omf","json","-rmns","%s","-rmd","%s"],"Type":"CUSTOM_JAR","ActionOnFailure":"CONTINUE","Jar":"command-runner.jar","Properties":"","Name":"%s"}]'
 
 	step=$(printf "$JSON_FMT" "$sparkParallel" "$seed" "$numTree" "$batchSize" "$mtryFraction" "$S3$prefix.csv.bz2" "$S3$prefix.pheno.no_.csv" "$S3R$prefixOut.vsis.csv" "$S3R$prefixOut.rf.json" "$minSample" "$maxDepth" "$prefixOut")
 
 	echo $step
+	echo "-------------------------"
 
 	aws emr add-steps --cluster-id $clusterID --steps $step
 
@@ -195,24 +205,26 @@ batchSize=100
 numVariant=1000000
 numSample=10000
 
-for mtry in 10000 5000 1000 500 100 50 10
+for mtry in 10 50 100 500 1000 5000 10000
 do
 	echo "========================="
-	echo "Variants:	$numVariant"
-	echo "Samples:	$numSample"
 
 	size=$((numSample*numVariant))
 	seed=$((mtry*21))
+	echo "seed: " $seed
 
 
 	prefix=dataset.$numSample.$numVariant
-	prefixOut=$experiment.mt$mtry
+	prefixOut=$(printf "$experiment.rmt%05d" $mtry)
+	echo "output: " $prefixOut
+	echo "-------------------------"
 
 	JSON_FMT='[{"Args":["spark-submit","--deploy-mode","client","--class","au.csiro.variantspark.cli.VariantSparkApp","/home/hadoop/biospark/lib/python3.6/site-packages/varspark/jars/variant-spark_2.11-0.3.0-SNAPSHOT-all.jar","importance","-io","{\"defVariableType\":\"ORDINAL(3)\"}","-sp","%s","-sr","%s","-v","-ro","-rn","%s","-rbs","%s","-rmt","%s","-it","csv","-if","%s","-fc","lable","-ff","%s","-of","%s","-on","1000000","-om","%s","-omf","json","-rmns","%s","-rmd","%s"],"Type":"CUSTOM_JAR","ActionOnFailure":"CONTINUE","Jar":"command-runner.jar","Properties":"","Name":"%s"}]'
 
 	step=$(printf "$JSON_FMT" "$sparkParallel" "$seed" "$numTree" "$batchSize" "$mtry" "$S3$prefix.csv.bz2" "$S3$prefix.pheno.no_.csv" "$S3R$prefixOut.vsis.csv" "$S3R$prefixOut.rf.json" "$minSample" "$maxDepth" "$prefixOut")
 
 	echo $step
+	echo "-------------------------"
 
 	aws emr add-steps --cluster-id $clusterID --steps $step
 
@@ -230,42 +242,41 @@ batchSize=100
 #numVariant=X
 #numSample=Y
 
-for numVariant in 10000 100000 1000000 10000000 100000000
+for numSample in 1000 10000 100000
 do
-
-	for numSample in 1000 10000 100000
+	for numVariant in 10000 100000 1000000 10000000 100000000
 	do
 		echo "========================="
-		echo "Variants:	$numVariant"
-		echo "Samples:	$numSample"
 
 		size=$((numSample*numVariant))
 		seed=$((size/12345))
+		echo "seed: " $seed
 
 		if [[ numSample -gt 10000 ]]
 		then
-			echo "10 batch"
 			batchSize=10
 		fi
 
-		if [[ size -gt 10000000000 ]]
+		if [[ size -gt 100000000000 ]]
 		then
-			echo "100 Trees"
 			numTree=100
 		fi
 
 		if [[ size -gt 1000000000000 ]]
 		then
-			echo "Dataset is too big"
+			echo "Epic Dataset (100K samples and 100M SNPs = 10TB) is generated and processed using separate script"
 		else
 			prefix=dataset.$numSample.$numVariant
-			prefixOut=$experiment.s$numSample.v$numVariant.nt$numTree.bs$batchSize
+			prefixOut=$(printf "$experiment.s%06d.v%09d.rn%04d.rbs%03d" $numSample $numVariant $numTree $batchSize)
+			echo "output: " $prefixOut
+			echo "-------------------------"
 
-		JSON_FMT='[{"Args":["spark-submit","--deploy-mode","client","--class","au.csiro.variantspark.cli.VariantSparkApp","/home/hadoop/biospark/lib/python3.6/site-packages/varspark/jars/variant-spark_2.11-0.3.0-SNAPSHOT-all.jar","importance","-io","{\"defVariableType\":\"ORDINAL(3)\"}","-sp","%s","-sr","%s","-v","-ro","-rn","%s","-rbs","%s","-rmtf","%s","-it","csv","-if","%s","-fc","lable","-ff","%s","-of","%s","-on","1000000","-om","%s","-omf","json","-rmns","%s","-rmd","%s"],"Type":"CUSTOM_JAR","ActionOnFailure":"CONTINUE","Jar":"command-runner.jar","Properties":"","Name":"%s"}]'
+			JSON_FMT='[{"Args":["spark-submit","--deploy-mode","client","--class","au.csiro.variantspark.cli.VariantSparkApp","/home/hadoop/biospark/lib/python3.6/site-packages/varspark/jars/variant-spark_2.11-0.3.0-SNAPSHOT-all.jar","importance","-io","{\"defVariableType\":\"ORDINAL(3)\"}","-sp","%s","-sr","%s","-v","-ro","-rn","%s","-rbs","%s","-rmtf","%s","-it","csv","-if","%s","-fc","lable","-ff","%s","-of","%s","-on","1000000","-om","%s","-omf","json","-rmns","%s","-rmd","%s"],"Type":"CUSTOM_JAR","ActionOnFailure":"CONTINUE","Jar":"command-runner.jar","Properties":"","Name":"%s"}]'
 
-		step=$(printf "$JSON_FMT" "$sparkParallel" "$seed" "$numTree" "$batchSize" "$mtryFraction" "$S3$prefix.csv.bz2" "$S3$prefix.pheno.no_.csv" "$S3R$prefixOut.vsis.csv" "$S3R$prefixOut.rf.json" "$minSample" "$maxDepth" "$prefixOut")
+			step=$(printf "$JSON_FMT" "$sparkParallel" "$seed" "$numTree" "$batchSize" "$mtryFraction" "$S3$prefix.csv.bz2" "$S3$prefix.pheno.no_.csv" "$S3R$prefixOut.vsis.csv" "$S3R$prefixOut.rf.json" "$minSample" "$maxDepth" "$prefixOut")
 
 			echo $step
+			echo "-------------------------"
 
 			aws emr add-steps --cluster-id $clusterID --steps $step
 		fi
