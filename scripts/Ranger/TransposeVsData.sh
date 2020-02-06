f31c46da363f695a6353cb693175abc421f9b5e2
ArashBayatDev <Arash.Bayat@csiro.au>
2020-02-05 20:08:13 -0800

    fixes

--- scripts/Ranger/TransposeVsData.sh
+++ scripts/Ranger/TransposeVsData.sh
@@ -8,6 +8,7 @@ OUTPUT=$4
 set -x
 cut -f 2 -d , $PEHNO > lbl
 split -l $LINES $GENO
+rm $GENO
 set +x
 
 echo "Transpose each part"
@@ -15,15 +16,15 @@ for f in x*
 do
         echo $f
         datamash transpose -t , < $f > $f.t
+        rm $f
 done
 
 set -x
 
-paste x*.t | tr \\t , | cut -f 2-1000000000 -d , | paste lbl - | tr \\t , > $OUTPUT
+paste x*.t | tr \\t , | cut -f 2-1000000000 -d , | paste lbl - | tr \\t , | pigz -p 16 > $OUTPUT
 rm lbl x*
 
-/usr/bin/time -v  ranger --verbose --file $4 --depvarname lable --treetype 1 --ntree 100 --nthreads 8 --outprefix ranger.out 2>&1 | tee ranger.log
-
-/usr/bin/time -v  ranger --verbose --file $4 --depvarname lable --treetype 1 --ntree 100 --nthreads 8 --outprefix ranger.sm.out --savemem 2>&1 | tee ranger.sm.log
+# /usr/bin/time -v  ranger --verbose --file $4 --depvarname lable --treetype 1 --ntree 100 --nthreads 8 --outprefix ranger.out 2>&1 | tee ranger.log
+# /usr/bin/time -v  ranger --verbose --file $4 --depvarname lable --treetype 1 --ntree 100 --nthreads 8 --outprefix ranger.sm.out --savemem 2>&1 | tee ranger.sm.log
 
 exit
