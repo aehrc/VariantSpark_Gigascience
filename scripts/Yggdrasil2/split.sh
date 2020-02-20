8d3199dffd5ceb0473f7294187c55af50cce811a
ArashBayatDev <Arash.Bayat@csiro.au>
2020-02-19 16:19:25 -0800

    Ygggdrasil compeleted

--- scripts/Yggdrasil2/split.sh
+++ scripts/Yggdrasil2/split.sh
@@ -22,8 +22,16 @@ echo $vname
 aws s3 cp s3://variant-spark/GigaScience/Data/VSdata/$GENO - | pv | bzcat | tail -n +2 | grep -v -E "$vname" | cat lbl - | cut -f 2-1000000000 -d , | pbzip2 -p16 | aws s3 cp - s3://variant-spark/GigaScience/Data/Yggdrasil2/$OUTPUT
 
 
+aws s3 cp s3://variant-spark/GigaScience/Data/Yggdrasil2/$OUTPUT ./
 
-aws s3 cp s3://variant-spark/GigaScience/Data/Yggdrasil2/$OUTPUT - |tee >(bash Subset.sh $numSam 100 1) >(bash Subset.sh $numSam 200 1) >(bash Subset.sh $numSam 400 1) >(bash Subset.sh $numSam 800 1) >(bash Subset.sh $numSam 1600 2) >(bash Subset.sh $numSam 3200 2) >(bash Subset.sh $numSam 6400 2) >(bash Subset.sh $numSam 12800 2) >(bash Subset.sh $numSam 25600 2) >(bash Subset.sh $numSam 51200 2) >(bash Subset.sh $numSam 102400 4) >(bash Subset.sh $numSam 204800 8) >(bash Subset.sh $numSam 409600 8) >(bash Subset.sh $numSam 819200 8) >(bash Subset.sh $numSam 1638400 16) >(bash Subset.sh $numSam 3276800 16) |  bash Subset.sh $numSam 6553600 16
+v=100
+for i in {1..17}
+do
+        set -x
+        cat $OUTPUT | pv | pbzip2 -dc -p32 | bash Subset.sh $numSam $v 96
+        set +x
+        v=$(( v*2 ))
+done
 
 exit
 
